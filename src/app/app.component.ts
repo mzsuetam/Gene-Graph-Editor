1161695ee5af9ea510d5170b4619415718b4d75b
Mateusz Mazur <mazurm@student.agh.edu.pl>
2022-10-01 16:39:40 +0200

    Implemented tools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
+//////////////////////////////
 
+const NODE_TAG = "n"
+const EDGE_1_TAG = "e"
+const EDGE_2_TAG = "f"
 
+//////////////////////////////
+
+enum EdgeType { E, F }
+enum Tool { HAND, ADDNODE, ADDEDGE, EDIT }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,16 +22,22 @@ export class AppComponent {
     dragging_enabled: true,
 
     physical: {
+      sub_text: 3,
+      sup_text: -12,
       node_size: 60,
       safe_distance_factor: 3
     }
   }
+
+  tool : Tool = Tool.HAND
 
   mouse : Mouse = new Mouse;
 
   nodes : Node[] = [] as Node[]
   edges_1 : Edge[] = [] as Edge[]
   edges_2 : Edge[] = [] as Edge[]
+
+  selected_nodes : number[] = []
 
   constructor(){
 
@@ -38,31 +52,37 @@ export class AppComponent {
 
 
   ngOnInit(){
+    document.getElementById('canvas')!.addEventListener('click', (event) => {
+      if ( this.tool == Tool.ADDNODE ){
+        alert(this.mouse.x+" "+this.mouse.y)
+      }
+    });
+
     this.addNode()
     this.addNode()
     this.addNode()
-    this.addNode()
+    this.addNode({text:"bage", sub:"", sup:"4"})
     
-    this.addEdge1(0, 1)
-    this.addEdge1(0, 2)
-    this.addEdge1(1, 3)
-    this.addEdge1(2, 3)
+    this.addEdge(0, 1, EdgeType.E)
+    this.addEdge(0, 2, EdgeType.E)
+    this.addEdge(1, 3, EdgeType.E)
+    this.addEdge(2, 3, EdgeType.E)
     
-    this.addEdge2(3, 2)
-    this.addEdge2(3, 0)
-    this.addEdge2(2, 0)
+    this.addEdge(3, 2, EdgeType.F)
+    this.addEdge(3, 0, EdgeType.F)
+    this.addEdge(2, 0, EdgeType.F, {text:"text", sub:"sub", sup:"sup"})
   }
 
-  public async addNode(){
+  public async addNode(badge?: Badge){
     let id = this.nodes.length
-    let new_node = new Node(id, this.settings.physical.node_size)
+    let new_node = new Node(id, this.settings.physical.node_size, badge)
 
     this.nodes.push( new_node )
     
     await new Promise(f => setTimeout(f, 100));
 
     document.getElementById( "node-"+id )!.addEventListener('mousedown', (e) => {
-      if ( e.button == 0 ){
+      if ( this.tool == Tool.HAND && e.button == 0 ){
         var i = 0
         var int = setInterval(() => {
           //<-- actions when we hold the button
@@ -81,7 +101,6 @@ export class AppComponent {
               this.edges_1[new_node.edges_1_out[i]].physical.height -= dy
             }
             for( let i=0; i< new_node.edges_1_in.length; i++ ){
-              // this.edges[new_node.edges_out[i]].physical.width
               let dw = new_node.physical.x - this.edges_1[new_node.edges_1_in[i]].physical.x
               let dh = new_node.physical.y - this.edges_1[new_node.edges_1_in[i]].physical.y
               
@@ -101,7 +120,6 @@ export class AppComponent {
               this.edges_2[new_node.edges_2_out[i]].physical.height -= dy
             }
             for( let i=0; i< new_node.edges_2_in.length; i++ ){
-              // this.edges[new_node.edges_out[i]].physical.width
               let dw = new_node.physical.x - this.edges_2[new_node.edges_2_in[i]].physical.x
               let dh = new_node.physical.y - this.edges_2[new_node.edges_2_in[i]].physical.y
               
@@ -119,133 +137,137 @@ export class AppComponent {
       
         })
       }
-      else if ( e.button == 2 ){
+      else if ( this.tool == Tool.EDIT && e.button == 0 ){
         alert("Node details")
       }
 
     });
   }
 
-  public async addEdge1(start:number, end:number){
+  public async addEdge(start:number, end:number, type: EdgeType, badge?: Badge){
+    // @FIXME: merge this if's
+    if ( type == EdgeType.E ){
       let id = this.edges_1.length
-      let new_edge = new Edge(id, start, end, "1")
+      let new_edge = new Edge(id, start, end, type, badge)
+
       this.nodes[start].edges_1_out.push(id)
       this.nodes[end].edges_1_in.push(id)
   
       this.edges_1.push( new_edge )
-
+  
       await new Promise(f => setTimeout(f, 100));
-
-    document.getElementById( "edge-1-"+id+"-path" )!.addEventListener('mousedown', (e) => {
-      if ( e.button == 0 ){
-        var int = setInterval(() => {
-          //<-- actions when we hold the button
-          if ( this.settings.dragging_enabled ){
-            if ( !this.isEdge1Flipped(id) ){
-              let x0 = this.nodes[new_edge.start_node].physical.x
-              let y0 = this.nodes[new_edge.start_node].physical.y
-
-              let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
-              let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
-              let c = Math.sqrt( dx*dx + dy*dy )
-
-              new_edge.physical.v1_x = this.mouse.x - x0
-              new_edge.physical.v1_y = this.mouse.y - y0
   
-              new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.x - x0 :  new_edge.physical.v2_x
-              new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.y - y0 :  new_edge.physical.v2_y
-            }
-            else{
-              let x0 = this.nodes[new_edge.start_node].physical.x + new_edge.physical.width
-              let y0 = this.nodes[new_edge.start_node].physical.y + new_edge.physical.height
-
-              let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
-              let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
-              let c = Math.sqrt( dx*dx + dy*dy )
-              
-              new_edge.physical.v1_x = x0 - this.mouse.x
-              new_edge.physical.v1_y = y0 - this.mouse.y
+      document.getElementById( "edge-1-"+id+"-path" )!.addEventListener('mousedown', (e) => {
+        if ( this.tool == Tool.HAND &&  e.button == 0 ){
+          var int = setInterval(() => {
+            //<-- actions when we hold the button
+            if ( this.settings.dragging_enabled ){
+              if ( !this.isEdge1Flipped(id) ){
+                let x0 = this.nodes[new_edge.start_node].physical.x
+                let y0 = this.nodes[new_edge.start_node].physical.y
   
-              new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? x0 - this.mouse.x :  new_edge.physical.v2_x
-              new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? y0 - this.mouse.y :  new_edge.physical.v2_y
-
+                let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
+                let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
+                let c = Math.sqrt( dx*dx + dy*dy )
+  
+                new_edge.physical.v1_x = this.mouse.x - x0
+                new_edge.physical.v1_y = this.mouse.y - y0
+    
+                new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.x - x0 :  new_edge.physical.v2_x
+                new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.y - y0 :  new_edge.physical.v2_y
+              }
+              else{
+                let x0 = this.nodes[new_edge.start_node].physical.x + new_edge.physical.width
+                let y0 = this.nodes[new_edge.start_node].physical.y + new_edge.physical.height
+  
+                let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
+                let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
+                let c = Math.sqrt( dx*dx + dy*dy )
+                
+                new_edge.physical.v1_x = x0 - this.mouse.x
+                new_edge.physical.v1_y = y0 - this.mouse.y
+    
+                new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? x0 - this.mouse.x :  new_edge.physical.v2_x
+                new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? y0 - this.mouse.y :  new_edge.physical.v2_y
+  
+              }
             }
-          }
-        }, 10)
-      
-        document.addEventListener("mouseup", () => {
-          clearInterval(int);
-        //<-- actions when we release the button
-          // console.log("release")
-      
-        })
-      }
-      else if ( e.button == 2 ){
-        alert("Edge details")
-      }
-    });
-  }
-
-
-  public async addEdge2(start:number, end:number){
-    let id = this.edges_2.length
-    let new_edge = new Edge(id, start, end, "2")
-    this.nodes[start].edges_2_out.push(id)
-    this.nodes[end].edges_2_in.push(id)
-
-    this.edges_2.push( new_edge )
-
-    await new Promise(f => setTimeout(f, 100));
-
-  document.getElementById( "edge-2-"+id+"-path" )!.addEventListener('mousedown', (e) => {
-    if ( e.button == 0 ){
-      var int = setInterval(() => {
-        //<-- actions when we hold the button
-        if ( this.settings.dragging_enabled ){
-          if ( !this.isEdge2Flipped(id) ){
-            let x0 = this.nodes[new_edge.start_node].physical.x
-            let y0 = this.nodes[new_edge.start_node].physical.y
-
-            let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
-            let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
-            let c = Math.sqrt( dx*dx + dy*dy )
-
-            new_edge.physical.v1_x = this.mouse.x - x0
-            new_edge.physical.v1_y = this.mouse.y - y0
-
-            new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.x - x0 :  new_edge.physical.v2_x
-            new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.y - y0 :  new_edge.physical.v2_y
-          }
-          else{
-            let x0 = this.nodes[new_edge.start_node].physical.x + new_edge.physical.width
-            let y0 = this.nodes[new_edge.start_node].physical.y + new_edge.physical.height
-
-            let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
-            let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
-            let c = Math.sqrt( dx*dx + dy*dy )
-            
-            new_edge.physical.v1_x = x0 - this.mouse.x
-            new_edge.physical.v1_y = y0 - this.mouse.y
-
-            new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? x0 - this.mouse.x :  new_edge.physical.v2_x
-            new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? y0 - this.mouse.y :  new_edge.physical.v2_y
-
-          }
+          }, 10)
+        
+          document.addEventListener("mouseup", () => {
+            clearInterval(int);
+          //<-- actions when we release the button
+            // console.log("release")
+        
+          })
         }
-      }, 10)
-    
-      document.addEventListener("mouseup", () => {
-        clearInterval(int);
-      //<-- actions when we release the button
-        // console.log("release")
-    
-      })
+        else if ( this.tool == Tool.EDIT && e.button == 0 ){
+          alert("Edge details")
+        }
+      });
+
     }
-    else if ( e.button == 2 ){
-      alert("Edge details")
+    else if ( type == EdgeType.F){
+      let id = this.edges_2.length
+      let new_edge = new Edge(id, start, end, type, badge)
+      this.nodes[start].edges_2_out.push(id)
+      this.nodes[end].edges_2_in.push(id)
+  
+      this.edges_2.push( new_edge )
+  
+      await new Promise(f => setTimeout(f, 100));
+  
+      document.getElementById( "edge-2-"+id+"-path" )!.addEventListener('mousedown', (e) => {
+        if ( this.tool == Tool.HAND && e.button == 0 ){
+          var int = setInterval(() => {
+            //<-- actions when we hold the button
+            if ( this.settings.dragging_enabled ){
+              if ( !this.isEdge2Flipped(id) ){
+                let x0 = this.nodes[new_edge.start_node].physical.x
+                let y0 = this.nodes[new_edge.start_node].physical.y
+  
+                let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
+                let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
+                let c = Math.sqrt( dx*dx + dy*dy )
+  
+                new_edge.physical.v1_x = this.mouse.x - x0
+                new_edge.physical.v1_y = this.mouse.y - y0
+  
+                new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.x - x0 :  new_edge.physical.v2_x
+                new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? this.mouse.y - y0 :  new_edge.physical.v2_y
+              }
+              else{
+                let x0 = this.nodes[new_edge.start_node].physical.x + new_edge.physical.width
+                let y0 = this.nodes[new_edge.start_node].physical.y + new_edge.physical.height
+  
+                let dx = Math.abs( this.mouse.x - this.nodes[new_edge.end_node].physical.x )
+                let dy = Math.abs( this.mouse.y - this.nodes[new_edge.end_node].physical.y )
+                let c = Math.sqrt( dx*dx + dy*dy )
+                
+                new_edge.physical.v1_x = x0 - this.mouse.x
+                new_edge.physical.v1_y = y0 - this.mouse.y
+  
+                new_edge.physical.v2_x = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? x0 - this.mouse.x :  new_edge.physical.v2_x
+                new_edge.physical.v2_y = ( c > this.settings.physical.node_size * this.settings.physical.safe_distance_factor ) ? y0 - this.mouse.y :  new_edge.physical.v2_y
+  
+              }
+            }
+          }, 10)
+        
+          document.addEventListener("mouseup", () => {
+            clearInterval(int);
+          //<-- actions when we release the button
+            // console.log("release")
+        
+          })
+        }
+        else if ( this.tool == Tool.EDIT && e.button == 0 ){
+          alert("Edge details")
+        }
+      });
     }
-  });
-}
+
+  }
 
   isEdge1Flipped(e : number) : boolean{
     let edge = this.edges_1[e]
@@ -279,6 +301,58 @@ export class AppComponent {
     return c > this.settings.physical.node_size 
   }
 
+  async flipSelection(n : number){
+    if ( this.tool != Tool.ADDEDGE ) return
+    if ( this.isSelected(n) ){
+      this.selected_nodes = this.selected_nodes.filter(x => x != n)
+    }
+    else{
+      this.selected_nodes.push(n)
+    }
+    if ( this.selected_nodes.length == 2 ) {
+      // @TODO: handle add edge
+      await new Promise(f => setTimeout(f, 500)); // tmp
+
+      this.selected_nodes = []
+    }
+  }
+
+  isSelected(n : number): boolean{
+    let out = this.selected_nodes.filter(x => x == n) // only 1 because we store id's
+    return out.length > 0 ? true : false
+  }
+
+  setTool(t: Tool){
+    this.tool = t
+    if ( this.tool != Tool.ADDEDGE ){
+      this.selected_nodes = []
+    }
+  }
+
+  getCursorStyle(x: string) : string{
+    if ( this.tool == Tool.HAND ){
+      if ( x == 'canvas' ) return 'default'
+      else if ( x == 'node' ) return 'move'
+      else if ( x == 'edge' ) return 'sw-resize'
+    }
+    else if ( this.tool == Tool.EDIT ){
+      if ( x == 'canvas' ) return 'default'
+      else if ( x == 'node' ) return 'crosshair'
+      else if ( x == 'edge' ) return 'crosshair'
+    }
+    else if ( this.tool == Tool.ADDNODE ){
+      if ( x == 'canvas' ) return 'pointer'
+      else if ( x == 'node' ) return 'inherit'
+      else if ( x == 'edge' ) return 'inherit'
+    }
+    else if ( this.tool == Tool.ADDEDGE ){
+      if ( x == 'canvas' ) return 'default'
+      else if ( x == 'node' ) return 'pointer'
+      else if ( x == 'edge' ) return 'default'
+    }
+    return ""
+  }
+
 }
 
 export class Mouse {
@@ -294,6 +368,7 @@ export class Mouse {
 
 export class Node{
   id : number;
+  badge : Badge;
 
   edges_1_out : number[] = []
   edges_1_in : number[] = []
@@ -307,10 +382,21 @@ export class Node{
     width : 0
   } as PhysicalNodeProperties
 
-  constructor(n: number, d: number){
+  constructor(n: number, d: number, badge ?: Badge){
     this.id =  n    
     this.physical.width = d
     this.physical.height = d
+    if ( badge ){
+      this.badge = badge
+    }
+    else{
+      let b = {
+        text: NODE_TAG,
+        sub: n.toString(),
+        sup: ""
+      } as Badge
+      this.badge = b
+    }
   }
 }
 
@@ -323,7 +409,8 @@ export interface PhysicalNodeProperties{
 
 export class Edge{
   id : number;
-  type : string; // A, B or C
+  type : EdgeType;
+  badge : Badge;
 
   start_node: number;
   end_node: number;
@@ -339,11 +426,31 @@ export class Edge{
     v2_y : 0
   } as PhysicalEdgeProperties
 
-  constructor(n: number, start:number, end:number, type: string){
+  constructor(n: number, start:number, end:number, type: EdgeType, badge?: Badge){
     this.id =  n   
     this.start_node = start 
     this.end_node = end 
     this.type = type
+
+    if ( badge ){
+      this.badge = badge
+    }
+    else if ( type == EdgeType.E ){
+      let b = {
+        text: EDGE_1_TAG,
+        sub: n.toString(),
+        sup: ""
+      } as Badge
+      this.badge = b
+    }
+    else { //} if ( type == EdgeType.F ){
+      let b = {
+        text: EDGE_2_TAG,
+        sub: n.toString(),
+        sup: ""
+      } as Badge
+      this.badge = b
+    }
   }
   
 }
@@ -357,4 +464,10 @@ export interface PhysicalEdgeProperties{
   v1_y : number;
   v2_x : number;
   v2_y : number;
+}
+
+export interface Badge{
+  text: string;
+  sub: string;
+  sup: string;
 }
